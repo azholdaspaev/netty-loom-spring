61c2c11e018eb81254522b918ea9e8d1ffd3c8a1
azholdaspaev <adilzholdaspaev@gmail.com>
2026-01-27 00:15:08 +0300

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+allprojects {
+    group = "io.github.azholdaspaev"
+    version = "0.1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    plugins.withType<JavaPlugin> {
+        extensions.configure<JavaPluginExtension> {
+            toolchain {
+                languageVersion.set(JavaLanguageVersion.of(24))
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "netty-loom-spring"
+
+include("netty-loom-spring-core")
+include("netty-loom-spring-mvc")
+include("netty-loom-spring-boot-starter")
+include("netty-loom-spring-example-netty")
+include("netty-loom-spring-example-tomcat")
