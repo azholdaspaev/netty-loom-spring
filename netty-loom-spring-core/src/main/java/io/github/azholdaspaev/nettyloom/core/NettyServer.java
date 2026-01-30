ba9d534af89765a06ef1c0858da13d0e7e502c4a
azholdaspaev <adilzholdaspaev@gmail.com>
2026-01-30 14:58:25 +0300

    Simple http server

---
+++ netty-loom-spring-core/src/main/java/io/github/azholdaspaev/nettyloom/core/NettyServer.java
@@ -0,0 +1,44 @@
+package io.github.azholdaspaev.nettyloom.core;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+public class NettyServer {
+
+    private EventLoopGroup bossGroup;
+    private EventLoopGroup workerGroup;
+
+    public synchronized void start() throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new HttpServerCodec());
+                        pipeline.addLast(new HttpObjectAggregator(65536));
+                        pipeline.addLast(new HelloWorldHandler());
+                    }
+                });
+
+        ChannelFuture channelFuture = bootstrap.bind(8080).sync();
+        channelFuture.channel().closeFuture().sync();
+    }
+
+    public synchronized void stop() {
+        bossGroup.shutdownGracefully();
+        workerGroup.shutdownGracefully();
+    }
+}
